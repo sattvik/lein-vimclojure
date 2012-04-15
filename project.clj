2e401a5513b3cbf86ed1fcaad72891ed9180dae3
Daniel Solano Gómez <daniel@solanogomez.org>
2012-04-14 23:24:54 -0500

    Plug-in has moved

--- project.clj
+++
@@ -1,7 +0,0 @@
-(defproject com.sattvik/lein-vimclojure "0.9.1-SNAPSHOT"
-  :description "Launches a VimClojure server for Vim."
-  :url "https://github.com/sattvik/lein-vimclojure"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[trammel "0.7.0"]]
-  :eval-in-leiningen true)
